1b32026402eab3206c1f0d19e609e58ea976eb37
Shunsuke024 <shunsuke44440416@gmail.com>
2023-12-13 23:38:00 +0900

    update layout

--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -3,9 +3,8 @@
     <p class="text-center text-xl mb-5">非表示ボタンを押すことで単語を非表示にできます</p>
     <form class="" action="/" method="get">
         @csrf
-        <div class="flex justify-around mt-5">
-            <div class="flex items-center mr-96">
-                <p>表示数：</p>
+        <div class="flex justify-around text-center items-center mb-5 ml-5">
+            <div>
                 <select id="displaynumber" name="num">
                     <option value="10" @if($number == "10") selected @endif>10</option>
                     <option value="20" @if($number == "20") selected @endif>20</option>
@@ -13,11 +12,6 @@
                     <option value="50" @if($number == "50") selected @endif>50</option>
                     <option value="100" @if($number == "100") selected @endif>100</option>
                 </select>
-            </div>
-            <div></div>
-        </div>
-        <div class="flex justify-around text-center items-center mb-5 ml-5">
-            <div>
                 <select id="category" name="categories">
                     <option value="all"  >すべて</option>
                     @foreach($categories as $category)
@@ -39,22 +33,22 @@
     
     <div class="words mt-5 mb-5 py-8 bg-sky-100">
         <div class="text-center mb-5">
-        <button class="bg-blue-400 hover:bg-blue-500 text-white rounded px-2 py-1 shadow-md" onclick="allhide_left()">すべて非表示</button>
-        <button class="bg-blue-400 hover:bg-blue-500 text-white rounded px-2 py-1 shadow-md" onclick="allhide_right()">すべて非表示</button>
+        <button class="bg-blue-400 hover:bg-blue-500 text-white rounded px-2 py-1 shadow-md" onclick="allhide_left()">すべて隠す</button>
+        <button class="bg-blue-400 hover:bg-blue-500 text-white rounded px-2 py-1 shadow-md" onclick="allhide_right()">すべて隠す</button>
         </div>
         @foreach ($words as $i => $word)
             <div class="flex justify-center items-center">
-                <button class="bg-blue-400 hover:bg-blue-500 text-white rounded px-2 py-1 ml-32 shadow-md" id="button1_{{ $i }}">非表示</button>
-                <div class="flex basis-1/2 items-stretch">
-                    <p class="flex justify-center basis-1/2 border items-center border-2 border-blue-100 bg-white text-xl mb-px mr-px whitespace-pre-wrap" id="left_{{ $i }}">{{ $word->word_left }}</p>
-                    <p class="flex justify-center basis-1/2 border items-center border-2 border-blue-100 bg-white text-xl mb-px whitespace-pre-wrap" id="right_{{ $i }}">{{ $word->word_right }}</p>
+                <button class="basisi-1/12 bg-blue-400 hover:bg-blue-500 text-white rounded px-2 py-1 shadow-md" id="button1_{{ $i }}">隠す</button>
+                <div class="flex basis-2/3 items-stretch">
+                    <p class="flex basis-1/2 justify-center border items-center border-2 border-blue-100 bg-white text-xl mb-px mr-px whitespace-pre-wrap" id="left_{{ $i }}">{{ $word->word_left }}</p>
+                    <p class="flex basis-1/2 justify-center border items-center border-2 border-blue-100 bg-white text-xl mb-px whitespace-pre-wrap" id="right_{{ $i }}">{{ $word->word_right }}</p>
                 </div>
-                <button class="bg-blue-400 hover:bg-blue-500 text-white rounded px-2 py-1 shadow-md" id="button2_{{ $i }}">非表示</button>
+                <button class="basisi-1/12 bg-blue-400 hover:bg-blue-500 text-white rounded px-2 py-1 shadow-md" id="button2_{{ $i }}">隠す</button>
                 
-                <div class="basis-1/10 ml-16">
+                <div class="basis-1/12 text-center">
                     <a class="" href="/words/{{ $word->id }}/edit">編集</a>
                 </div>
-                <div class="basis-1/10">
+                <div class="basis-1/12 text-center">
                 <form action="/words/{{ $word->id }}" id="form_{{ $word->id }}" method="post">
                     @csrf
                     @method('DELETE')
